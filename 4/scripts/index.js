61e885de5903c8248abe846dc66d4f51c773b1fe
Keks <keks@htmlacademy.ru>
2023-12-21 22:53:51 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/scripts/index.js
@@ -0,0 +1,1 @@
+document.getElementById("burger").addEventListener("click",()=>{document.querySelector(".menu-nav__list").classList.toggle("active"),document.querySelector(".menu-nav__toggle").classList.toggle("active")});
