d666aadcb27852e3e022377d331e884d4a6e3673
brazilets <brazilets161@gmail.com>
2024-01-11 20:06:00 +0300

    Добавляет no-js в выпадающее меню

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,3 +1,6 @@
+document.querySelector('.header').classList.remove('header--no-js');
+document.querySelector('.menu-nav').classList.remove('menu-nav--no-js');
+
 document.getElementById('burger').addEventListener('click', () => {
   document.querySelector('.menu-nav__list').classList.toggle('active');
   document.querySelector('.menu-nav__toggle').classList.toggle('active');
