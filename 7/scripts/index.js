bb0531f99b0cf7636bf06538bd03934f64c21ac7
Keks <keks@htmlacademy.ru>
2024-01-19 19:28:12 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/scripts/index.js
@@ -0,0 +1,1 @@
+document.querySelector(".header").classList.remove("header--nojs");document.querySelector(".menu-nav").classList.remove("menu-nav--nojs");document.getElementById("burger").addEventListener("click",()=>{document.querySelector(".menu-nav__list").classList.toggle("active"),document.querySelector(".menu-nav__toggle").classList.toggle("active")});
