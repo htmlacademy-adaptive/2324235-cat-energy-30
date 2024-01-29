bdbcd92555a6c6ad01651c41de43ea316ba3012a
brazilets <brazilets161@gmail.com>
2024-01-29 19:45:59 +0300

    Добавляет правки по результатам первой проверки

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,5 +1,6 @@
 document.querySelector('.header').classList.remove('header--nojs');
 document.querySelector('.menu-nav').classList.remove('menu-nav--nojs');
+document.querySelector('.contacts__maps').classList.remove('contacts__maps--nojs');
 
 document.getElementById('burger').addEventListener('click', () => {
   document.querySelector('.menu-nav__list').classList.toggle('active');
